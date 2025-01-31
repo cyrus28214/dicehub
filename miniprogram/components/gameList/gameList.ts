dd84119cdd1f68792e3fed9c2897f84043c5a055
Cyrus28214 <cyrus@gmail.com>
2025-01-31 22:08:30 +0800

    feat: gameDetail页面

--- miniprogram/components/gameList/gameList.ts
+++ miniprogram/components/gameList/gameList.ts
@@ -9,6 +9,7 @@ Component({
   methods: {
     onGameTap(e: WechatMiniprogram.CustomEvent) {
       const game = e.currentTarget.dataset.game;
+      console.debug("onGameTap", game);
       this.triggerEvent('gameselect', { gameId: game.id });
     }
   }
---
+++ miniprogram/pages/gameDetail/gameDetail.ts
@@ -0,0 +1,29 @@
+import { Game, games } from '../../data/games';
+
+Component({
+  data: {
+    game: null as Game | null
+  },
+  methods: {
+    onLoad(options: any) {
+        const gameId = parseInt(options?.gameId);
+        const game = games.find(g => g.id === gameId);
+        
+        if (game) {
+        this.setData({ game });
+        } else {
+        wx.showToast({
+            title: '游戏不存在',
+            icon: 'error'
+        });
+        wx.navigateBack();
+        }
+    },
+    onShareTap() {
+      wx.showShareMenu({
+        withShareTicket: true,
+        menus: ['shareAppMessage', 'shareTimeline']
+      });
+    }
+  }
+}); 
