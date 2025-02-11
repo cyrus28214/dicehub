e6c74ff1357a1a9fc417ae8719270ebfdd246e3f
Lice <42827113+LiceWx@users.noreply.github.com>
2025-02-12 06:41:21 +0800

    分类系统
    - 添加了“分类”页面（category）
    - 对于每个分类，添加了该类的介绍页（categoryDetail）。其中包括类别的基本信息以及相关游戏（依据是games.ts中的tag，但这个过程是ai分类的，似乎存在一些差错。不过这都是临时数据，如果有后端的话直接使用代码逻辑筛选就不会错了）。
    - 介绍页中的游戏复用了 gameList 组件，可以点进去。
    - 主页中四个 TRPG 改成了随机抽取 cates.ts（类别的数据）中的四个分类。不过类型对应的icon还没搞。

---
+++ wx-miniprogram/miniprogram/components/gameList/gameList.ts
@@ -0,0 +1,17 @@
+Component({
+  properties: {
+    games: {
+      type: Array,
+      value: []
+    }
+  },
+
+  methods: {
+    onGameTap(e: WechatMiniprogram.CustomEvent) {
+      const game = e.currentTarget.dataset.game;
+      wx.navigateTo({
+        url: `/pages/gameDetail/gameDetail?gameId=${game.id}`
+      });
+    },
+  }
+}); 
