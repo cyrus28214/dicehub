831f426142e270a3bb713497b4a3eec3d4a24885
cyrus <cyrus28214@gmail.com>
2025-02-12 17:05:16 +0800

    fix

--- miniprogram/pages/category/category.ts
+++ miniprogram/pages/category/category.ts
@@ -1,37 +1,22 @@
-Component({
-    data: {
-        type: '',
-        title: '',
-        games: []
-    },
+import { cates, Category　} from '../../data/cates';
 
+Page({
+  data: {
+    title: "主题浏览",
+    cates: [] as Category[]
+  },
 
-    methods: {
-      onLoad(options: any) {
-          const type = options.type
-          this.setData({ type, title: type })
-      },
+  onLoad() {
+    this.setData({
+      cates: cates
+    })
+    console.log('分类数据：', this.data.cates)  // 用于调试
+  },
 
-      onGameSelect(e: WechatMiniprogram.CustomEvent) {
-          const gameId = e.detail
-          wx.navigateTo({
-              url: `/pages/gameDetail/gameDetail?gameId=${gameId}`
-          })
-      },
-
-      onShareAppMessage() {
-        const promise = new Promise(resolve => {
-          setTimeout(() => {
-            resolve({
-              title: '桌游助手-分类'
-            });
-          }, 2000); // 延迟 2 秒返回分享内容
-        });
-        return {
-          title: '桌游助手-分类', // 默认转发标题
-          path: '/pages/category/category', // 转发页面路径
-          promise // 异步处理内容（如需要动态生成转发标题）
-        };
-      },
-    }
+  onCategoryTap(e: any) {
+    const { item } = e.detail
+    wx.navigateTo({
+      url: `/pages/categoryDetail/categoryDetail?id=${item.id}`
+    })
+  }
 }) 
